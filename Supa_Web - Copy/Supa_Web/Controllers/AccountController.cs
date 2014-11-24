c05b04f9290f231dcc4a4b9c7d4cc170ffdc13c4
Elderry <Elderry@outlook.com>
2014-11-24 22:38:47 +0800

    Updated DB

--- Supa_Web - Copy/Supa_Web/Controllers/AccountController.cs
+++ Supa_Web - Copy/Supa_Web/Controllers/AccountController.cs
@@ -5,6 +5,9 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
 using Supa_Web.Models;
 
 namespace Supa_Web.Controllers
---
+++ Supa_Web - Copy/Supa_Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Supa_Web.Startup))]
+namespace Supa_Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
