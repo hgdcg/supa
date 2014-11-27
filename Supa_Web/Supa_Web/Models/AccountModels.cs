8d283d40d033d4f9062f8daf4f39a456d354a94e
Elderry <Elderry@outlook.com>
2014-11-27 21:05:51 +0800

    Added some verification

--- Supa_Web/Supa_Web/Models/AccountModels.cs
+++ Supa_Web/Supa_Web/Models/AccountModels.cs
@@ -3,12 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Supa_Web.Models
 {
     public class LogInModel
     {
+        [Required(ErrorMessage="用户名不能为空")]
         public String UserName { get; set; }
+        [Required(ErrorMessage="密码不能为空")]
         public String Password { get; set; }
     }
 
